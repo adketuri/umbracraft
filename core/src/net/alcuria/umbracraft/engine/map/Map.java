c2a8f8248d9cf8487aa1b8b0100db27e05517daa
Andrew Keturi <adketuri@gmail.com>
2015-04-26 09:56:01 -0700

    Place edges and walls

--- core/src/net/alcuria/umbracraft/engine/map/Map.java
+++ core/src/net/alcuria/umbracraft/engine/map/Map.java
@@ -45,22 +45,22 @@ public class Map implements Disposable {
 		layers = new Array<Layer>();
 		altMap = new int[width][height];
 
-		// set dummy alts
-		for (int i = 3; i < 9; i++) {
+		//set dummy alts
+		for (int i = 5; i < 7; i++) {
 			for (int j = 3; j < 9; j++) {
-				altMap[i][j] = 2;
+				altMap[i][j] = 1;
 			}
 		}
 		for (int i = 5; i < 7; i++) {
 			for (int j = 5; j < 7; j++) {
-				altMap[i][j] = 4;
+				altMap[i][j] = 2;
 			}
 		}
 		//---
-		altMap[10][10] = 4;
-		altMap[10][11] = 4;
-		altMap[9][10] = 4;
-		altMap[9][11] = 4;
+		//		altMap[10][10] = 4;
+		//		altMap[10][11] = 4;
+		//		altMap[9][10] = 4;
+		//		altMap[9][11] = 4;
 
 		// create list of all heights
 		Array<Integer> heights = new Array<Integer>();
@@ -73,46 +73,111 @@ public class Map implements Disposable {
 		}
 		heights.sort();
 
+		// build the layer
+		int lastAlt = 0;
 		for (Integer altitude : heights) {
 			Layer l = new Layer();
 			l.alt = altitude;
 			l.data = new Tile[width][height];
 			for (int i = 0; i < altMap.length; i++) {
 				for (int j = 0; j < altMap[0].length; j++) {
-					if (altMap[i][j] == altitude) {
+					if (altMap[i][j] >= altitude) {
 						if (j + altitude >= 0 && j + altitude < altMap[0].length) {
-							l.data[i][j + altitude] = new Tile(l.alt, l.alt);
-							if (j - 1 >= 0 && j - 1 < altMap[0].length) { // if in range
-								int drop = altMap[i][j] - altMap[i][j - 1];
+							l.data[i][j + altitude] = new Tile(createEdge(i, j, altitude), l.alt); // create edge
+							// check if we need to create a wall
+							if (j - 1 >= 0 && j - 1 < altMap[0].length) {
+								int drop = altitude - altMap[i][j - 1];
 								while (drop > 0) {
-									l.data[i][(j + altitude) - drop] = new Tile(def.middleCenterWall, l.alt);
+									l.data[i][(j + altitude) - drop] = new Tile(createWall(i, j, drop, altitude, lastAlt), l.alt);
 									drop--;
 								}
 							}
-							//							for (int k = 1; k <= altitude; k++) {
-							//								if (altMap[i][j - k] < altitude - altMap[i][j]) {
-							//									l.data[i][(j + altitude) - k] = new Tile(def.middleCenterWall, l.alt);
-							//								}
-							//							}
 						}
 					}
 				}
 			}
 			layers.add(l);
+			lastAlt = l.alt;
 		}
 
-		//		for (int i = 0; i < data.length; i++) {
-		//			for (int j = 0; j < data[0].length; j++) {
-		//				if (altMap[i][j] > curAlt) {
-		//					int alt = altMap[i][j];
-		//					data[i][j - alt].id = getEdgeData(i, j);
-		//					while (alt > 0) {
-		//						alt--;
-		//						data[i][j - alt].id = getWallData(i, j, alt);
-		//					}
+		//		// fill in missing tiles underneath edges
+		//		for (int i = 0; i < altMap.length; i++) {
+		//			for (int j = 0; j < altMap[0].length; j++) {
+		//				if (layers.get(0).data[i][j] == null) {
+		//					layers.get(0).data[i][j] = new Tile(def.floor, layers.get(0).alt);
 		//				}
 		//			}
 		//		}
+
+	}
+
+	private boolean altContains(int i, int j) {
+		return i >= 0 && i < altMap.length && j >= 0 && j < altMap[0].length;
+	}
+
+	private int createEdge(int i, int j, int altitude) {
+		if (def == null) {
+			return 0;
+		}
+		// top right down left
+		int mask = 0b0000;
+		if (altContains(i, j + 1) && altMap[i][j + 1] < altitude) {
+			mask = mask ^ 0b1000;
+		}
+		if (altContains(i + 1, j) && altMap[i + 1][j] < altitude) {
+			mask = mask ^ 0b0100;
+		}
+		if (altContains(i, j - 1) && altMap[i][j - 1] < altitude) {
+			mask = mask ^ 0b0010;
+		}
+		if (altContains(i - 1, j) && altMap[i - 1][j] < altitude) {
+			mask = mask ^ 0b0001;
+		}
+		// now to switch on every possibility
+		switch (mask) {
+		case 0b0001:
+			return def.edgeLeft;
+		case 0b0010:
+			return def.edgeBottom;
+		case 0b0100:
+			return def.edgeRight;
+		case 0b1000:
+			return def.edgeTop;
+		case 0b1100:
+			return def.edgeTopRight;
+		case 0b1001:
+			return def.edgeTopLeft;
+		case 0b0110:
+			return def.edgeBottomRight;
+		case 0b0011:
+			return def.edgeBottomLeft;
+		}
+		return 0;
+	}
+
+	private int createWall(int i, int j, int drop, int altitude, int lastAlt) {
+		Game.log(i + " " + j + " " + " " + drop + " " + altitude);
+		if (drop + lastAlt == altitude) {
+			// lower walls
+			Game.log("placing low");
+			if (altContains(i - 1, j) && altMap[i - 1][j] < altitude) {
+				return def.bottomLeftWall;
+			} else if (altContains(i + 1, j) && altMap[i + 1][j] < altitude) {
+				return def.bottomRightWall;
+			} else {
+				return def.bottomCenterWall;
+			}
+		} else {
+			Game.log("placing mid");
+			// upper walls
+			if (altContains(i - 1, j) && altMap[i - 1][j] < altitude) {
+				return def.middleLeftWall;
+			} else if (altContains(i + 1, j) && altMap[i + 1][j] < altitude) {
+				return def.middleRightWall;
+			} else {
+				return def.middleCenterWall;
+			}
+		}
 	}
 
 	@Override
@@ -138,7 +203,7 @@ public class Map implements Disposable {
 	/** Renders every visible layer. */
 	public void render() {
 		final int tileSize = Game.config().tileWidth;
-		for (int k = 0; k < layers.size; k++) {
+		for (int k = 0; k < 3; k++) {
 			final Tile[][] data = layers.get(k).data;
 			for (int i = 0; i < data.length; i++) {
 				for (int j = 0; j < data[0].length; j++) {
