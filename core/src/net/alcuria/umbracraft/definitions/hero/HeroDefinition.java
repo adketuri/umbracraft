154ac41a538eaf1c089a327ae282e061ee66ee86
Andrew Keturi <adketuri@gmail.com>
2015-07-26 16:23:22 -0700

    Document HeroDefinition

--- core/src/net/alcuria/umbracraft/definitions/hero/HeroDefinition.java
+++ core/src/net/alcuria/umbracraft/definitions/hero/HeroDefinition.java
@@ -5,8 +5,11 @@ import net.alcuria.umbracraft.definitions.Definition;
 /** Defines a single hero.
  * @author Andrew Keturi */
 public class HeroDefinition extends Definition {
+	/** The hero's HP */
 	public int hp;
+	/** The hero's max level */
 	public int maxLevel;
+	/** the hero's name */
 	public String name;
 
 	@Override
