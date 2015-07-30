96bc05144fb985db7c14fe2490248e96620e4d8b
Andrew Keturi <adketuri@gmail.com>
2015-07-30 07:29:10 -0700

    Show tile edges on map editor

--- core/src/net/alcuria/umbracraft/editor/widget/MapTileWidget.java
+++ core/src/net/alcuria/umbracraft/editor/widget/MapTileWidget.java
@@ -15,7 +15,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MapTileWidget extends Table {
 
-	private static TextureRegion side, top;
+	private static TextureRegion side, top, edge;
 	private int altitude = 0;
 	private final MapDefinition definition;
 	private final int i, j;
@@ -27,6 +27,7 @@ public class MapTileWidget extends Table {
 		if (side == null) {
 			Texture skin = new Texture(Gdx.files.internal("editor/skin.png"));
 			side = new TextureRegion(skin, 4, 0, 1, 1);
+			edge = new TextureRegion(skin, 2, 0, 1, 1);
 			top = new TextureRegion(skin, 3, 0, 1, 1);
 		}
 		setBackground(Drawables.get("blue"));
@@ -53,10 +54,35 @@ public class MapTileWidget extends Table {
 		setTouchable(Touchable.enabled);
 	}
 
+	private int alt(int i, int j) {
+		if (i < 0 || i >= definition.tiles.length || j < 0 || j >= definition.tiles[0].length) {
+			return 0;
+		}
+		return definition.tiles[i][j].altitude;
+	}
+
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
+		// side
 		batch.draw(side, getX(), getY(), getWidth(), getWidth() * altitude);
+		// top
 		batch.draw(top, getX(), getY() + altitude * getHeight(), getWidth(), getHeight());
+		// left edge
+		if (alt(i - 1, j) < alt(i, j)) {
+			batch.draw(edge, getX(), getY() + altitude * getHeight(), 2, getHeight());
+		}
+		// right edge
+		if (alt(i + 1, j) < alt(i, j)) {
+			batch.draw(edge, getX() + getWidth() - 2, getY() + altitude * getHeight(), 2, getHeight());
+		}
+		// bottom edge
+		if (alt(i, j + 1) < alt(i, j)) {
+			batch.draw(edge, getX(), getY() + altitude * getHeight(), getWidth(), 2);
+		}
+		// top edge
+		if (alt(i, j - 1) < alt(i, j)) {
+			batch.draw(edge, getX(), getY() + getHeight() + altitude * getHeight() - 2, getWidth(), 2);
+		}
 	}
 }
