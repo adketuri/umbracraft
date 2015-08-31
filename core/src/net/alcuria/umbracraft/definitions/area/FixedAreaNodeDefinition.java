81d979a8a4dbeda9213fb4e31ce15d9ea3420093
Andrew Keturi <adketuri@gmail.com>
2015-08-31 07:44:01 -0700

    Subclass AreaNodeDefinitions

---
+++ core/src/net/alcuria/umbracraft/definitions/area/FixedAreaNodeDefinition.java
@@ -0,0 +1,8 @@
+package net.alcuria.umbracraft.definitions.area;
+
+/** Defines a fixed node.
+ * @author Andrew Keturi */
+public class FixedAreaNodeDefinition extends AreaNodeDefinition {
+	/** the map definition to use */
+	public String mapDefinition;
+}
---
+++ core/src/net/alcuria/umbracraft/definitions/area/RandomAreaNodeDefinition.java
@@ -0,0 +1,15 @@
+package net.alcuria.umbracraft.definitions.area;
+
+/** Defines a random node.
+ * @author Andrew Keturi */
+public class RandomAreaNodeDefinition extends AreaNodeDefinition {
+	/** the area map's height */
+	public int height;
+	/** the area map's variance */
+	public int heightVariance;
+	/** the area map's width */
+	public int width;
+	/** the aream map's variance (width) */
+	public int widthVariance;
+
+}
