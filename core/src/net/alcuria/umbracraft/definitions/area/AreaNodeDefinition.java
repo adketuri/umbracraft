74bcd69064e017b6836077517ffe517dbcfd2ece
Andrew Keturi <adketuri@gmail.com>
2015-09-01 07:53:01 -0700

    Revert AreaNodeDefinition subclassing

--- core/src/net/alcuria/umbracraft/definitions/area/AreaNodeDefinition.java
+++ core/src/net/alcuria/umbracraft/definitions/area/AreaNodeDefinition.java
@@ -7,11 +7,20 @@ import com.badlogic.gdx.utils.Array;
 /** A single node on the {@link AreaDefinition}.
  * @author Andrew Keturi */
 public class AreaNodeDefinition extends Definition {
-
 	/** all children nodes for the area */
 	public Array<AreaNodeDefinition> children;
+	/** the area map's height */
+	public int height;
+	/** the area map's variance */
+	public int heightVariance;
+	/** the map definition to use */
+	public String mapDefinition;
 	/** the name of the area map */
 	public String name;
+	/** the area map's width */
+	public int width;
+	/** the aream map's variance (width) */
+	public int widthVariance;
 
 	@Override
 	public String getName() {
--- core/src/net/alcuria/umbracraft/definitions/area/FixedAreaNodeDefinition.java
+++
@@ -1,8 +0,0 @@
-package net.alcuria.umbracraft.definitions.area;
-
-/** Defines a fixed node.
- * @author Andrew Keturi */
-public class FixedAreaNodeDefinition extends AreaNodeDefinition {
-	/** the map definition to use */
-	public String mapDefinition;
-}
