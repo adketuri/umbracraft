81dd7604b6f61bbb764811b6c53cede6dec748c5
Andrew Keturi <adketuri@gmail.com>
2015-09-24 08:11:31 -0700

    Update to latest libgdx version

--- core/src/net/alcuria/umbracraft/engine/windows/message/MessageWindowLayout.java
+++ core/src/net/alcuria/umbracraft/engine/windows/message/MessageWindowLayout.java
@@ -14,8 +14,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 import com.kotcrab.vis.ui.widget.VisLabel;
 
