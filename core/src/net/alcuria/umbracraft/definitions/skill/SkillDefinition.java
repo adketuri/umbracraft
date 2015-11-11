258d5068e1b80df973d8454d02f19bb5eb9c71b7
Andrew Keturi <adketuri@gmail.com>
2015-11-11 08:34:37 -0800

    Add fields for targets and actions

--- core/src/net/alcuria/umbracraft/definitions/skill/SkillDefinition.java
+++ core/src/net/alcuria/umbracraft/definitions/skill/SkillDefinition.java
@@ -2,11 +2,14 @@ package net.alcuria.umbracraft.definitions.skill;
 
 import net.alcuria.umbracraft.annotations.Tooltip;
 import net.alcuria.umbracraft.definitions.Definition;
+import net.alcuria.umbracraft.definitions.skill.actions.SkillActionDefinition;
 
 import com.badlogic.gdx.utils.Array;
 
 public class SkillDefinition extends Definition {
 
+	@Tooltip("The action sequence when using the skill")
+	public Array<SkillActionDefinition> actions;
 	@Tooltip("The multiplier of the base damage")
 	public float damageMultiplier;
 	@Tooltip("The cost to use the skill, in EP")
@@ -17,7 +20,7 @@ public class SkillDefinition extends Definition {
 	@Tooltip("The name of the skill")
 	public String name;
 	@Tooltip("The targets of the skill, relative to the drop point")
-	public Array<SkillPositionDefinition> target;
+	public Array<SkillPositionDefinition> targets;
 	@Tooltip("The ATB cost of the skill, in turns")
 	public float turnCost;
 
