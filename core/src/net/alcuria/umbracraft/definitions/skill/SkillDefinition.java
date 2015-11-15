9b9a22aea3511ecf85fca190b993ca80ca6d3f84
Andrew Keturi <adketuri@gmail.com>
2015-11-15 15:27:40 -0800

    Add skill targeting restriction definitions to SkillDefinition

--- core/src/net/alcuria/umbracraft/definitions/skill/SkillDefinition.java
+++ core/src/net/alcuria/umbracraft/definitions/skill/SkillDefinition.java
@@ -8,6 +8,10 @@ import com.badlogic.gdx.utils.Array;
 
 public class SkillDefinition extends Definition {
 
+	public static enum TargetRestriction {
+		NO_RESTRICTION, REQUIRES_FREE_SPACE, REQUIRES_TARGET
+	}
+
 	@Tooltip("The action sequence when using the skill")
 	public Array<SkillActionDefinition> actions;
 	@Tooltip("The multiplier of the base damage")
@@ -16,9 +20,10 @@ public class SkillDefinition extends Definition {
 	public int epCost;
 	@Tooltip("The icon to use for the skill")
 	public String iconId;
-	public float maxPercentageCost;
 	@Tooltip("The name of the skill")
 	public String name;
+	@Tooltip("Any restrictions on using the skill. For instance, the skill requires a target to cast or the skill must be cast into a blank space.")
+	public TargetRestriction targetRestriction = TargetRestriction.NO_RESTRICTION;
 	@Tooltip("The targets of the skill, relative to the drop point")
 	public Array<SkillPositionDefinition> targets;
 	@Tooltip("The ATB cost of the skill, in turns")
