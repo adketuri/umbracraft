5ac61a419b87ae93048359248f514fb73a8424aa
Andrew Keturi <adketuri@gmail.com>
2016-03-30 08:23:44 -0700

    Add WindowTable javadocs

--- core/src/net/alcuria/umbracraft/engine/windows/WindowTable.java
+++ core/src/net/alcuria/umbracraft/engine/windows/WindowTable.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
 
+/** A table with the background set to the game's window styling. Use
+ * {@link WindowTable#put(Actor)} to add things to the table.
+ * @author Andrew Keturi */
 public class WindowTable extends Table {
 
 	private Table content;
@@ -30,6 +33,9 @@ public class WindowTable extends Table {
 
 	}
 
+	/** Equivalent to add, but puts the content in the correct spot.
+	 * @param actor the actor to add to the table.
+	 * @return a {@link Cell} */
 	public <T extends Actor> Cell<T> put(T actor) {
 		return content.add(actor);
 	}
