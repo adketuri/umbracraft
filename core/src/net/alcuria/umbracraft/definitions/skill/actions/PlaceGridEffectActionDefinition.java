cb01eecefb99b622da3d7a7f9732fe7902a9f008
Andrew Keturi <adketuri@gmail.com>
2016-07-25 08:48:14 -0700

    Place field effect parameters

--- core/src/net/alcuria/umbracraft/definitions/skill/actions/PlaceGridEffectActionDefinition.java
+++ core/src/net/alcuria/umbracraft/definitions/skill/actions/PlaceGridEffectActionDefinition.java
@@ -1,9 +1,16 @@
 package net.alcuria.umbracraft.definitions.skill.actions;
 
+import net.alcuria.umbracraft.annotations.Tooltip;
+
 public class PlaceGridEffectActionDefinition extends SkillActionDefinition {
 
+	@Tooltip("An animation displayed on-tile")
 	public String animation;
+	@Tooltip("A unique id. Not sure what this will do yet.")
 	public String id;
+	@Tooltip("ID of the skill to invoke for damage, etc, on tick")
+	public String skillId;
+	@Tooltip("The duration of the effect")
 	public int turns;
 
 	@Override
@@ -12,6 +19,7 @@ public class PlaceGridEffectActionDefinition extends SkillActionDefinition {
 		def.animation = animation;
 		def.id = id;
 		def.turns = turns;
+		def.skillId = skillId;
 		return def;
 	}
 }
